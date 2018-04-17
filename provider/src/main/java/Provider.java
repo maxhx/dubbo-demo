0ceecf5aca78ccee199dc28598d8933cd4332715
MAIHX <MAIHX@MAIHX100.cn.Midea.com>
2018-04-17 19:15:22 +0800

    dubbo-provider 服务提供者示例

---
+++ provider/src/main/java/Provider.java
@@ -0,0 +1,13 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Provider {
+	
+	public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] {"application-dubbo-provider.xml"});
+        context.start();
+        // press any key to exit
+        System.in.read();
+    }
+
+}
